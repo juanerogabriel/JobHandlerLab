b8db88b586354448c9464dd5503d02d7ec9d9182
juanerogabriel <juan.davila8@upr.edu>
2018-08-29 20:44:32 -0400

    Added seniorityLevel method

--- src/Employee.java
+++ src/Employee.java
@@ -82,35 +82,17 @@ public class Employee {
 	public int seniorityLevel() {
 		
 		//Add Code Here [You have to use If/Else to get graded]
-		
-		return 0;// Temporal Return
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		if (yearService >= 10){
+			return 3;
+		}
+		else if(yearService <10 && yearService >= 5){
+			return 2;
+		}
+		else if (yearService < 5 && yearService >= 1){
+			return 1;
+		}
+		else{
+			return 0;// Temporal Return
+		}
+	}	
 }
